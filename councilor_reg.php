d9017d4d614b567939022475fc5b578f3a8f231e
Bikash Chowdhury <chowdhurybikash38@gmail.com>
2022-09-08 11:24:28 +0600

    country data and phone code updated

--- councilor_reg.php
+++ councilor_reg.php
@@ -128,7 +128,6 @@ include_once "./layout/navigation_bar.php";
                     <label for="country" class="form-label">Country</label>
                     <select name="country" class="form-select" id="country" required>
                         <option disabled value="">Choose...</option>
-
                     </select>
                     <div class="invalid-feedback">
                         Please select country.
@@ -139,7 +138,6 @@ include_once "./layout/navigation_bar.php";
                     <label for="phone-code" class="form-label">Code</label>
                     <select name="phone-code" class="form-select" id="phone-code" required>
                         <option value="" selected disabled>Choose...</option>
-                        <option value="phone-code">+880</option>
                     </select>
                     <div class="invalid-feedback">
                         lease select your valid phone code.
@@ -288,57 +286,57 @@ if (db_connection()->query($sql)) {
 
 
 <script type="text/javascript">
-    let country, countryName, countryId, phoneCode;
+    // css element selection 
+    let country = document.querySelector("#country");
+    let phoneCode = document.querySelector('#phone-code');
 
-    country = document.querySelector("#country");
-
-    // country = country.;
-    phoneCode = document.querySelector('#phone-code');
-
-    let newElement, attribute, content;
-
-
+    // collection of data 
     let country_data_collection = <?= json_encode(mysqli_fetch_all($country_data_collection, MYSQLI_ASSOC)) ?>;
+
+    // creating html element 
     for (let value of country_data_collection) {
 
+        let newElement;
         // country data 
         newElement = document.createElement('option');
         newElement.textContent = value.country_name;
         newElement.setAttribute("value", value.country_id);
         country.append(newElement);
-        // console.log(value);
+
 
         // phone code 
         newElement = document.createElement('option');
         newElement.textContent = value.phonecode;
-        newElement.setAttribute("value", value.phonecode);
+        newElement.setAttribute("value", value.country_id);
         phoneCode.append(newElement);
     }
 
+    // elements attributes 
     country.firstElementChild.setAttribute('selected', '');
     phoneCode.firstElementChild.setAttribute('selected', '');
 
 
-    // left by here =====================================
-    country.addEventListener('change', (e) => {
+    //Auto select event =====================================
+    country.addEventListener("change", (e) => {
         let selectedCountry = e.target.value;
+        phoneCode.firstElementChild.removeAttribute("selected");
 
-        for (value of country_data_collection) {
-            if (value.country_id == selectedCountry) {
-                console.log(phoneCode);
-                break;
+        let count = 1;
+        for (value in country_data_collection) {
+            let newNode;
+            newNode = document.createTextNode(country_data_collection[value].phonecode);
+
+            //   replace node
+            let existedElement = phoneCode.children[count];
+            existedElement.replaceChild(newNode, existedElement.childNodes[0]);
+
+            //   attributes
+            existedElement.removeAttribute("selected");
+            if (country_data_collection[value].country_id == selectedCountry) {
+                existedElement.setAttribute("selected", "");
+                console.log(country_data_collection[value].country_id);
             }
+            count++;
         }
-
-
     });
-
-
-
-
-
-
-
-    console.log(country);
-    console.log(phoneCode);
 </script>
